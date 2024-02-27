da3f2c9d20b3db030191cd81a3549fcca8e9e791
Vodka479 <songwut2015@hotmail.com>
2024-02-27 16:44:52 +0700

    First build  my empty structure :tada:

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Vodka479/vodka-shop-tutorial
+
+go 1.21.4
---
+++ pkg/databases/migrations/000002_vodka-db.down.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+-- ลบข้อมูลใน tables
+TRUNCATE TABLE "users" CASCADE;  
+TRUNCATE TABLE "oauth" CASCADE;
+TRUNCATE TABLE "roles" CASCADE;
+TRUNCATE TABLE "products" CASCADE;
+TRUNCATE TABLE "categories" CASCADE;
+TRUNCATE TABLE "products_categories" CASCADE;
+TRUNCATE TABLE "images" CASCADE;
+TRUNCATE TABLE "orders" CASCADE;
+TRUNCATE TABLE "products_orders" CASCADE;
+
+--reset sequence เลขที่รันอัตโนมัติ
+SELECT SETVAL ((SELECT PG_GET_SERIAL_SEQUENCE('"roles"', 'id')), 1, FALSE);
+SELECT SETVAL ((SELECT PG_GET_SERIAL_SEQUENCE('"categories"', 'id')), 1, FALSE);
+
+COMMIT;
